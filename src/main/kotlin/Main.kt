becec11b464a3730e25f74154545eefc507b20ad
Azat Azmetov <azmetoff@gmail.com>
2022-09-11 13:34:40 +0300

    Переделал в соответствии с рекомендациями ревьюера. Также вывел название пунктов меню в сами sealed - подклассы.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,113 @@
+import java.util.*
+import kotlin.system.exitProcess
+
 fun main() {
-    ScreenHelper.showScreen(Screen.ShowAllArchives)
+    ScreenNavigator().showScreen(Screen.ShowAllArchives)
 }
+
+class ScreenNavigator {
+    private val scanner = Scanner(System.`in`)
+    private val archives = mutableListOf<Archive>()
+
+    fun showScreen(screen: Screen) {
+        when (screen) {
+            is Screen.ShowAllArchives -> {
+                println(screen.MENU_TITLE)
+                println(screen.MENU_BUTTON_0)
+                printListInMenu(archives, screen.EXIT)
+                validateSymbolsInput()
+                val userValue: Int = scanner.nextInt()
+                navigateScreen(
+                    userValue, archives,
+                    callbackIfZero = { showScreen(Screen.CreateArchive) },
+                    callbackIfLast = { exitProcess(0) },
+                    callbackIfListItem = { showScreen(Screen.ShowArchive(archives[it])) },
+                    callbackIfError = { showScreen(Screen.ShowAllArchives) }
+                )
+            }
+            is Screen.ShowArchive -> {
+                println("${screen.MENU_TITLE} ${screen.archive.name}")
+                println(screen.MENU_BUTTON_0)
+                printListInMenu(screen.archive.notes, screen.EXIT)
+                validateSymbolsInput()
+                val userValue = scanner.nextInt()
+                val notes = screen.archive.notes
+                navigateScreen(
+                    userValue, notes,
+                    callbackIfZero = { showScreen(Screen.CreateNote(screen.archive)) },
+                    callbackIfLast = { showScreen(Screen.ShowAllArchives) },
+                    callbackIfListItem = { showScreen(Screen.ShowNote(notes[it])) },
+                    callbackIfError = { showScreen(Screen.ShowArchive(screen.archive)) }
+                )
+            }
+            is Screen.ShowNote -> {
+                println("${screen.MENU_TITLE} ${screen.note.title}")
+                println(screen.note.text)
+                println("${screen.ANY_KEY} - ${screen.EXIT}")
+                scanner.next()
+                showScreen(Screen.ShowArchive(screen.note.archive))
+            }
+
+            is Screen.CreateArchive -> {
+                println(screen.MENU_TITLE)
+                println(screen.ENTER_NAME)
+                val archiveName = scanner.next()
+                archives.add(Archive(archiveName))
+                showScreen(Screen.ShowAllArchives)
+
+            }
+            is Screen.CreateNote -> {
+                println(screen.MENU_TITLE)
+                print(screen.ENTER_NOTE_TITLE)
+                val title = scanner.next()
+                print(screen.ENTER_NOTE_TEXT)
+                val text = scanner.next()
+                screen.archive.notes.add(Note(title, text, screen.archive))
+                showScreen(Screen.ShowArchive(screen.archive))
+            }
+        }
+    }
+
+    private fun validateSymbolsInput() {
+        while (!scanner.hasNextInt()) {
+            println(ERROR_MESSAGE_ENTER_DIGIT)
+            scanner.next()
+        }
+    }
+
+    private fun navigateScreen(
+        userNumber: Int,
+        list: List<*>,
+        callbackIfZero: (() -> Unit),
+        callbackIfListItem: (Int) -> Unit,
+        callbackIfLast: () -> Unit,
+        callbackIfError: () -> Unit
+    ) {
+        when (userNumber) {
+            0 -> callbackIfZero()
+            list.size + 1 -> callbackIfLast()
+            in 1..(list.size) -> callbackIfListItem(userNumber - 1)
+            else -> {
+                showOutOfSizeError(list.size)
+                callbackIfError()
+            }
+        }
+    }
+
+    private fun showOutOfSizeError(maxSize: Int) {
+        println("$ERROR_MESSAGE_NUMBER_LIMITS ${maxSize + 1}")
+    }
+
+    private fun printListInMenu(list: List<PrintableName>, exitText: String) {
+        list.forEachIndexed { index, value ->
+            println("${index + 1}. ${value.getPrintableName()}")
+        }
+        println("${list.size + 1}. $exitText")
+    }
+
+    companion object {
+        const val ERROR_MESSAGE_NUMBER_LIMITS = "Число должно быть в пределах от 0 до"
+        const val ERROR_MESSAGE_ENTER_DIGIT = "Вы ввели не цифру! Введите цифру..."
+    }
+}
+
--- src/main/kotlin/Screen.kt
+++ src/main/kotlin/Screen.kt
@@ -1,7 +1,29 @@
 sealed class Screen {
-    object ShowAllArchives : Screen()
-    object CreateArchive : Screen()
-    class ShowArchive(val archive: Archive) : Screen()
-    class ShowNote(val note: Note) : Screen()
-    class CreateNote(val archive: Archive) : Screen()
+    object ShowAllArchives : Screen() {
+        const val MENU_TITLE = "ВСЕ АРХИВЫ"
+        const val MENU_BUTTON_0 = "0. Создать архив"
+    }
+
+    class ShowArchive(val archive: Archive) : Screen() {
+        val MENU_TITLE = "АРХИВ"
+        val MENU_BUTTON_0 = "0. Создать заметку"
+    }
+
+    class ShowNote(val note: Note) : Screen() {
+        val MENU_TITLE = "Заметка"
+    }
+
+    object CreateArchive : Screen() {
+        val MENU_TITLE = "СОЗДАНИЕ АРХИВА"
+        val ENTER_NAME = "Введите название архива: "
+    }
+
+    class CreateNote(val archive: Archive) : Screen() {
+        val MENU_TITLE = "СОЗДАНИЕ ЗАМЕТКИ"
+        val ENTER_NOTE_TITLE = "Введите название заметки: "
+        val ENTER_NOTE_TEXT = "Введите текст заметки: "
+    }
+
+    val EXIT = "Выход"
+    val ANY_KEY = "Любая клавиша"
 }
