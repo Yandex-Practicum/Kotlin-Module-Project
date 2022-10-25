9fd1dfb240a739e992e4dab0746910b2cfcf8bb7
mvrlrd <mvrlrd@yandex.ru>
2022-10-25 12:28:23 +0300

    started

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
+const val CHOOSE_ARCHIVE = "1 - Выбор архива"
+const val CREATE_ARCHIVE = "2 - Создание архива"
+const val EXIT = "0 - Выход"
+
 fun main() {
-    // ваш код начнется здесь
-    // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-    println("Привет")
+    val scanner = Scanner(System.`in`)
+    println(CHOOSE_ARCHIVE + "\n" +
+            CREATE_ARCHIVE + "\n" +
+            EXIT)
+
+    println(scanner.nextLine())
 }
