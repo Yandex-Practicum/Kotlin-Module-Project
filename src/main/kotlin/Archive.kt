e1f2e106735d355d78c83656c773011fa3d46e9d
NamerPRO <mail@namerpro.me>
2022-12-13 22:55:55 +0300

    работа №3 попытка 1

---
+++ src/main/kotlin/Archive.kt
@@ -0,0 +1,7 @@
+data class Archive(val archName: String, val content: MutableList<Note>) {
+
+    override fun toString(): String {
+        return archName
+    }
+
+}
---
+++ src/main/kotlin/Note.kt
@@ -0,0 +1,7 @@
+data class Note(val ntName: String, var data: String) {
+
+    override fun toString(): String {
+        return ntName
+    }
+
+}
