2ed6735d5a4b3ec4ad1a9de0f57142ea40a1de7d
Илья Корастылев <i.korastylev@yandex.ru>
2023-01-25 18:40:08 +0300

    Добавил обработку ввода символов в методе show класса Screen. Также поправил форматирование. В классе Main указал более лаконичный вызов метода show

--- src/main/kotlin/OpenNote.kt
+++ src/main/kotlin/OpenNote.kt
@@ -3,7 +3,7 @@ class OpenNote(private val note : Notes, private val parent : OpenArchive) {
     fun show() {
         while (true) {
             println("Выберите действие:\n0 - Показать заметку ${note.noteName}\n1 - Выход")
-            when (Scanner(System.`in`).nextInt()) {
+            when (Scanner(System.`in`).nextLine().toIntOrNull()) {
                 0 -> select()
                 1 -> parent.show()
                 else -> println("Введенное значение недопустимо. Введите значение 0 или 1")
