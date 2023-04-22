b04b4259ce0261cf4be3b210d63f05298ea2298f
Alex <39100738+AlexMargina@users.noreply.github.com>
2023-04-22 19:02:38 +0300

    Create MapNote.kt

---
+++ src/main/kotlin/MapNote.kt
@@ -0,0 +1,67 @@
+import java.util.Scanner
+
+class MapNotes (val mutableMapNotes: MutableMap<String, String>) {
+
+
+    //Создаем тестовые заметки и архивы
+    // val mapNotes  = mutableMapOf("Заметка #1" to "Значение заметки #1",
+    //     "Заметка #2" to "Значение заметки #2","Заметка #3" to "Значение заметки #3",)
+    fun addMapNotes(
+        mapNotes: MapNotes,
+        keyNote: String,
+        note: String
+    ): MutableMap<String, String> {
+        mapNotes.mutableMapNotes.plusAssign(keyNote to note)
+        return mapNotes.mutableMapNotes
+    }
+
+    fun makeNote(mapNotes: MapNotes) {
+        val input = Scanner(System.`in`)
+        println("Введите название заметки")
+        val keyNote = (input.nextLine())
+        println("Введите содержание заметки")
+        val note = (input.nextLine())
+        addMapNotes(mapNotes, keyNote, note)
+        println("Заметка добавлена")
+        val countmapNotes = mapNotes.mutableMapNotes.size
+        println("Теперь заметок: $countmapNotes")
+    }
+
+    fun addNote() {
+        val input = Scanner(System.`in`)
+        println("Введите название заметки")
+        val keyNote = (input.nextLine())
+        println("Введите содержание заметки")
+        val note = (input.nextLine())
+        mutableMapNotes.plusAssign(keyNote to note)
+        println("Заметка добавлена")
+        val countmapNotes = mutableMapNotes.size
+        println("Теперь заметок addNote(): $countmapNotes")
+
+    }
+}
+
+
+class MapArhiv (val mapArhiv : MutableMap<String,MapNotes>) {
+
+    //make arhive
+
+
+    fun addMapNoteToArhiv (keyArhiv:String) {
+        val mapNotesInArhiv = MapNotes( mutableMapOf<String, String>())
+        mapArhiv.plusAssign (keyArhiv to mapNotesInArhiv)
+    }
+
+    fun makeArhiv() {
+        val input = Scanner(System.`in`)
+        println("Введите название архива")
+        val keyArhiv = (input.nextLine())
+        val mapNotes: MutableMap<String, String> = mutableMapOf()
+
+        addMapNoteToArhiv (keyArhiv, )
+        println("Архив создан")
+        val countmapArhives = mapArhiv.size
+        println("Теперь количество архивов стало $countmapArhives")
+    }
+
+}
