cb3f6c831fe705250add9b0e85846d2710fd18d2
elik <lavriket@gmail.com>
2023-05-06 03:40:55 +0800

    Убрано дублирование в основном коде

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -1,6 +1,5 @@
 class Menu {
     fun screen(filename1: String): Boolean {
-        var line: String
         if (filename1 == "/src/main/kotlin") {
             println("Вы находитесь в исходной директории")
         } else {
@@ -16,22 +15,20 @@ class Menu {
         } else {
             println("5. Вернуться в исходную директорию")
         }
-        line = readln()
-        while (line != "1" && line != "2" && line != "3" && line != "4" && line != "5") {
+        var line = readln().toIntOrNull()
+        while (line !in 1..5) {
             println("Пожалуйста, выберите одну из предоставленных опций. Введите только цифру опции без других символов")
-            line = readln()
+            line = readln().toIntOrNull()
         }
-        if (line == "1") {
-            create(filename1, true)
-        } else if (line == "2") {
-            create(filename1, false)
-        } else if (line == "3") {
-            select(filename1, true)
-        } else if (line == "4") {
-            select(filename1, false)
-        } else if (line == "5") {
-            if (filename1 == "/src/main/kotlin") {
-                return false
+        when (line) {
+            1 -> create(filename1, true)
+            2 -> create(filename1, false)
+            3 -> select(filename1, true)
+            4 -> select(filename1, false)
+            5 -> {
+                if (filename1 == "/src/main/kotlin") {
+                    return false
+                }
             }
         }
         return true
@@ -39,121 +36,97 @@ class Menu {
 
     private fun select(filename1: String, isNote: Boolean) {
         var line: String
-        if (filename1 == "/src/main/kotlin") {
-            println("Содержание исходной директории:")
+        val altPrint = if (isNote) {
+            "заметки"
         } else {
-            println("Содержание архива '${currentFolder(filename1)}':")
+            "архива"
         }
-        search("NoMatter", filename1, true, "Search")
-        if (isNote) {
-            if (search("NoMatter",filename1, true, "IfEmpty")) {
-                println("Пожалуйста, выберите одну из предоставленных опций. Впишите название заметки")
-                line = readln()
-                if (search(line, filename1, true, "Check")) {
+        println("Содержание ${if (filename1 == "/src/main/kotlin") "исходной директории" else "архива '${currentFolder(filename1)}'"}:")
+        search("NoMatter", filename1, isNote, "Search")
+        if (search("NoMatter", filename1, isNote, "IfEmpty")) {
+            println("Пожалуйста, выберите одну из предоставленных опций. Впишите название $altPrint")
+            line = readln()
+            if (search(line, filename1, isNote, "Check")) {
+                if (isNote) {
                     readTxt(line, filename1)
                 } else {
-                    while (!search(line, filename1, true, "Check")) {
-                        println("Название заметки не было введено корректно. Попробуйте снова")
-                        line = readln()
-                    }
-                    readTxt(line, filename1)
+                    screen(selectArchive(line, filename1))
                 }
             } else {
-                if (filename1 == "/src/main/kotlin") {
-                    println("В этой директории нет заметок. Проверьте архивы")
-                    println()
-                } else {
-                    println("В архиве '${currentFolder(filename1)}' отсутсвуют заметки. Чтобы их выбрать, их нужно сначала создать")
-                    println()
+                while (!search(line, filename1, isNote, "Check")) {
+                    println("Название $altPrint не было введено корректно. Попробуйте снова")
+                    line = readln()
                 }
+                if (isNote) {
+                    readTxt(line, filename1)
+                } else {
+                    screen(selectArchive(line, filename1))
+                }
+
             }
         } else {
-            if (search("NoMatter",filename1, false, "IfEmpty")) {
-                println("Пожалуйста, выберите одну из предоставленных опций. Впишите название архива")
-                line = readln()
-                if (search(line, filename1, false, "Check")) {
-                    val filename = selectArchive(line, filename1)
-                    screen(filename)
-                } else {
-                    while (!search(line, filename1, false, "Check")) {
-                        println("Название архива не было введено корректно. Попробуйте снова")
-                        line = readln()
-                    }
-                    val filename = selectArchive(line, filename1)
-                    screen(filename)
-                }
-            }else{
-                if (filename1 == "/src/main/kotlin") {
-                    println("В этой директории нет архивов.")
-                    println()
-                } else {
-                    println("В архиве '${currentFolder(filename1)}'' отсутсвуют архивы. Чтобы их выбрать, их нужно сначала создать")
-                    println()
-                }
+            if (filename1 == "/src/main/kotlin") {
+                println("В этой директории нет ${if (isNote) "заметок. Проверьте архивы." else "архивов."}")
+                println()
+            } else {
+                println("В архиве '${currentFolder(filename1)}' отсутсвуют ${if (isNote) altPrint else "архивы"}. Чтобы их выбрать, их нужно сначала создать.")
+                println()
             }
         }
     }
 
     private fun create(filename1: String, isNote: Boolean) {
-        var line: String
-        if (isNote) {
-            var title: String
-            var texxt: String
-            println("Введите название заметки")
-            title = readln()
-            if (title == "") {
-                while (title == "") {
-                    println("Название заметки не может быть пустым, попробуйте снова")
-                    title = readln()
-                }
-            } else if (search(title, filename1, true, "Check")) {
-                while (search(title, filename1, true, "Check") || title == "") {
-                    println("Заметка с данным названием уже существует. Попробуйте снова.")
-                    title = readln()
-                }
+        var line: Int?
+        var title: String
+        var altPrint = if (isNote) {
+            "заметки"
+        } else {
+            "архива"
+        }
+        var texxt: String
+        println("Введите название $altPrint")
+        title = readln()
+        if (title == "") {
+            while (title == "") {
+                println("Название $altPrint не может быть пустым, попробуйте снова")
+                title = readln()
             }
-            println("Введите текст заметки")
+        } else if (search(title, filename1, isNote, "Check")) {
+            while (search(title, filename1, isNote, "Check") || title == "") {
+                println("${if (isNote) "Заметка" else "Архив"} с данным названием уже существует. Попробуйте снова.")
+                title = readln()
+            }
+        }
+        if (isNote) {
+            altPrint = "Введите текст заметки"
+            println(altPrint)
             texxt = readln()
-
             while (true) {
                 println("Пожалуйста, выберите опцию из предоставленных ниже")
                 println("1. Завершить создание заметки")
                 println("2. Продолжить текст с нового абзаца")
 
-                line = readln()
-                while (line != "1" && line != "2") {
+                line = readln().toIntOrNull()
+                while (line !in 1..2) {
                     println("Пожалуйста, выберите одну из предоставленных опций. Введите только цифру опции без других символов")
-                    line = readln()
+                    line = readln().toIntOrNull()
                 }
 
-                if (line == "1") {
+                if (line == 1) {
                     break
                 } else {
-                    println("Введите текст заметки")
+                    println(altPrint)
                     texxt = texxt + """|
                         |""".trimMargin() + readln()
                 }
             }
-
             createTxt(title, texxt, filename1)
         } else {
-            println("Введите название архива")
-            line = readln()
-            if (line == "") {
-                while (line == "") {
-                    println("Название архива не может быть пустым, попробуйте снова")
-                    line = readln()
-                }
-            } else if (search(line, filename1, false, "Check")) {
-                while (search(line, filename1, false, "Check")|| line == "") {
-                    println("Архив с данным названием уже существует. Попробуйте снова.")
-                    line = readln()
-                }
-            }
-            createArchive(line, filename1)
+            createArchive(title, filename1)
             println("Архив создан")
-            screen(selectArchive(line, filename1))
+            screen(selectArchive(title, filename1))
         }
+
     }
 }
 
@@ -162,15 +135,15 @@ fun currentFolder(filename1: String): String {
     var result = ""
     var truth = true
     for (i in array.indices.reversed()) { // итерируемся по массиву в обратном направлении
-        if(array[i]=='/'){
-            truth=false
+        if (array[i] == '/') {
+            truth = false
         }
-        if (truth){
+        if (truth) {
             result += array[i]
         }
     }
-    array=result.toCharArray()
-    result=""
+    array = result.toCharArray()
+    result = ""
     for (i in array.indices.reversed()) { // итерируемся по массиву в обратном направлении
         result += array[i]
     }
