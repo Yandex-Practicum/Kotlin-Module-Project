15854648a6835bdebe302f8f39f43e3185eceeaf
G-dev-ui <koroliov2108@mail.ru>
2023-11-02 13:20:08 +0300

    Консольное Приложение "Заметки"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val app = NotesApp()
+    app.run()
 }
---
+++ src/main/kotlin/Note.kt
@@ -0,0 +1,1 @@
+data class Note(val name: String, val content: String)
