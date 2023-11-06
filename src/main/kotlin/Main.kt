d449e11b9b8f2f27817ada7209720dc0341850db
Anton <antderar1488_antnoman2517@mail.ru>
2023-11-06 09:21:48 +0300

    Третьи изменения

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,35 +4,3 @@ fun main() {
     menu.showViewMenu(MenuArchiveCollectionView())
 }
 
-interface ViewInterface {
-    val title: String
-    val mapOfMenu: MutableMap<String, () -> Unit>
-
-    fun prepareToView ()
-}
-
-object NoteArchiveCollection {
-
-    val noteArchiveCollection: MutableMap<Int, NoteArchive> = mutableMapOf()
-
-    fun addToCollection (name: String) {
-        noteArchiveCollection[noteArchiveCollection.size] = NoteArchive(name)
-    }
-
-}
-
-class NoteArchive(
-    val name: String,
-    val noteArchive: MutableMap<Int, Note> = mutableMapOf()
-) {
-    fun addToNoteArchive (str: String) {
-        noteArchive[(noteArchive.size + 1)] = Note(str)
-    }
-
-}
-
-data class Note (
-    val textOfNote: String
-) {
-
-}
