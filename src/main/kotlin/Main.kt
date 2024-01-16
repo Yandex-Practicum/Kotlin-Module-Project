0fda23a023d244e34c126185209b25414fbb6e3f
Nikita_Grafov <grafstory@yandex.ru>
2024-01-16 22:19:01 +0300

    Добавил возможность добавлять содержимое заметки и просматривать соддержимое заметки. Класс Note перенес в отдельный файл. Исправил ситуацию с номером выхода из "Заметок" и "Архива". Теперь это последняя цифра в списке.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,16 @@
 import java.util.Scanner
 
-class Note(val text: String)
 
-class Archive(val name: String) {
-    val notes = mutableListOf<Note>()
-    fun addNote(note: Note) {
-        notes.add(note)
-    }
-}
 
 object NotesApp {
-    val archives = mutableListOf<Archive>()
-    val scanner = Scanner(System.`in`)
+    private val archives = mutableListOf<Archive>()
+    private val scanner = Scanner(System.`in`)
 
-    fun addArchive(archive: Archive) {
+    private fun addArchive(archive: Archive) {
         archives.add(archive)
     }
 
-    fun createArchive() {
+    private fun createArchive() {
         println("Введите название нового архива (или введите 2 для отмены):")
         val archiveName = scanner.nextLine()
         when {
@@ -36,15 +29,16 @@ object NotesApp {
         }
     }
 
-    fun selectArchiveMenu() {
+    private fun selectArchiveMenu() {
         while (true) {
             println("Список архивов:")
+            println("0. Создать новый архив")
             archives.forEachIndexed { index, archive ->
                 println("${index + 1}. ${archive.name}")
             }
-            println("0. Создать новый архив")
-            println("2. Выход")
-            println("Выберите архив (введите номер или 0 для создания нового, 2 для выхода):")
+
+            println("${archives.size + 1}. Выход")
+            println("Выберите архив (введите номер или 0 для создания нового, ${archives.size + 1} для выхода):")
             val input = scanner.nextLine()
 
             when {
@@ -70,23 +64,24 @@ object NotesApp {
         }
     }
 
-    fun selectNoteMenu(archive: Archive) {
+    private fun selectNoteMenu(archive: Archive){
         while (true) {
             println("Архив: ${archive.name}")
             println("Список заметок:")
-            archive.notes.forEachIndexed { index, note ->
-                println("${index + 1}. ${note.text}")
-            }
+            val lastNoteIndex = archive.notes.size  // Определяем номер последней заметки
             println("0. Создать новую заметку")
-            println("2. Вернуться к выбору архива")
-            println("Выберите заметку (введите номер, 0 для создания новой или 2 для выхода):")
+            archive.notes.forEachIndexed { index, note ->
+                println("${index + 1}. ${note.title}")
+            }
+            println("${lastNoteIndex + 1}. Вернуться к выбору архива")  // Используем последний номер заметки
+            println("Выберите заметку (введите номер, 0 для создания новой или ${lastNoteIndex + 1} для возврата):")
             val input = scanner.nextLine()
 
             when {
                 input == "0" -> {
                     createNote(archive)
                 }
-                input == "2" -> {
+                input == "${lastNoteIndex + 1}" -> {
                     println("Возврат к выбору архива.")
                     return
                 }
@@ -99,34 +94,37 @@ object NotesApp {
                     }
                 }
                 else -> {
-                    println("Некорректный ввод. Введите номер заметки, 0 для создания новой или 2 для возврата.")
+                    println("Некорректный ввод. Введите номер заметки, 0 для создания новой или ${lastNoteIndex + 1} для возврата.")
                 }
             }
         }
     }
 
-    fun createNote(archive: Archive) {
-        println("Введите текст новой заметки (или введите 2 для отмены):")
-        val noteText = scanner.nextLine()
+    private fun createNote(archive: Archive) {
+        println("Введите заголовок новой заметки (или введите 2 для отмены):")
+        val noteTitle = scanner.nextLine()
         when {
-            noteText == "2" -> {
+            noteTitle == "2" -> {
                 println("Создание заметки отменено.")
                 return
             }
-            noteText.isNotBlank() -> {
-                val newNote = Note(noteText)
+            noteTitle.isNotBlank() -> {
+                println("Введите содержимое новой заметки:")
+                val noteContent = scanner.nextLine()
+                val newNote = Note(noteTitle, noteContent)
                 archive.addNote(newNote)
-                println("Заметка \"$noteText\" добавлена в архив.")
+                println("Заметка \"$noteTitle\" добавлена в архив.")
             }
             else -> {
-                println("Текст заметки не может быть пустым.")
+                println("Заголовок заметки не может быть пустым.")
             }
         }
     }
 
-    fun viewNoteContent(note: Note) {
+    private fun viewNoteContent(note: Note) {
         println("Содержимое заметки:")
-        println(note.text)
+        println("Заголовок: ${note.title}")
+        println("Содержание: ${note.content}")
     }
 
     @JvmStatic
