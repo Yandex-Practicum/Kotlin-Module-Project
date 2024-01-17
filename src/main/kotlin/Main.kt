455793509eda75875499f84bf72947d16c23c9e5
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2024-01-17 06:45:27 +0600

    add under control first verion project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,39 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val scan = Scanner(System.`in`)
+
+    while (true){
+        println("1.Выбрать архив\n2.Создать архив\n3.Выйти из программы")
+
+        when(scan.nextLine().toInt()){
+            1 -> selectArch()
+            2 -> createArch()
+            3 -> break
+        }
+
+    }
+}
+
+fun selectArch(){
+    println("1.Выбрать заметку\n2.Создать заметку")
+
+    val scan = Scanner(System.`in`)
+
+    when(scan.nextLine().toInt()){
+        1 -> selectNote()
+        2 -> createNote()
+    }
+}
+
+fun createArch(){
+    println("Создать архив")
+}
+
+fun selectNote(){
+    println("Выбираем заметку")
+}
+
+fun createNote(){
+    println("Создаем заметку")
 }
