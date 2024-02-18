10223291af835ee1c573df86164592d7788453b2
Kirill.Smolyakov <smolyakov.kirusha@gmail.com>
2024-02-19 02:00:33 +0600

    add under control func getMenuCorrectInput() and getStringCorrectInput()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,26 @@
 import Data.Archieve
 import Data.Note
+import PaigeLogic
 import java.util.Scanner
+
 
 val archievePage = PaigeLogic()
 
 fun main() {
     while (true){
         println("1.Выбрать архив\n2.Создать архив\n3.Выйти из программы")
+        var value = archievePage.getMenuCorrectInput()
 
-        val scan = Scanner(System.`in`)
-        val value = scan.nextLine()
-        if(archievePage.isInputCorrect(value)){
-            when(value.toInt()){
-                1 -> selectArch()
-                2 -> createArch()
-                3 -> break
-                else -> println("Выберите элемент меню")
-            }
-        }
-        else {
-            println("Необходимо ввести число")
-            continue
+        when(value){
+            1 -> selectArch()
+            2 -> createArch()
+            3 -> break
+            else -> println("Выберите элемент меню")
         }
     }
 }
 
 fun selectArch(){
-
-    val scan = Scanner(System.`in`)
 
     println("Список архивов")
     if (archievePage.showAllArchieves() == null) {
@@ -36,34 +29,29 @@ fun selectArch(){
     }
 
     println(archievePage.showAllArchieves())
-    var archieveID = scan.nextLine().toInt()
+    var archieveID = archievePage.getMenuCorrectInput()
     println(archievePage.chooseArchieve(archieveID)?.name)
 
     while (true){
         println("1.Выбрать заметку\n2.Создать заметку\n3.Выйти в меню Архивов")
-        var noteID = scan.nextLine()
-        if (archievePage.isInputCorrect(noteID)){
-            when(noteID.toInt()){
-                1 -> archievePage.chooseArchieve(archieveID)?.let { selectNote(it) }
-                2 -> archievePage.chooseArchieve(archieveID)?.let { createNote(it) }
-                3 -> break
-                else -> println("Выберите элемент меню")
-            }
-        }
-        else {
-            println("Необходимо ввести число")
-            continue
+        var noteID = archievePage.getMenuCorrectInput()
+
+        when(noteID){
+            1 -> archievePage.chooseArchieve(archieveID)?.let { selectNote(it) }
+            2 -> archievePage.chooseArchieve(archieveID)?.let { createNote(it) }
+            3 -> break
+            else -> println("Выберите элемент меню")
         }
     }
+
 }
 
 fun createArch(){
 
     println("Создаем архив")
 
-    val scan = Scanner(System.`in`)
     println("Введите имя архива:")
-    val archName: String = scan.nextLine().toString()
+    val archName: String = archievePage.getStringCorrectInput()
 
     val archieve = Archieve(archName)
 
@@ -80,11 +68,10 @@ fun selectNote(archieve: Archieve) {
 
     println("Для просмотра заметки введите ее номер")
 
-    val scan = Scanner(System.`in`)
 
     println(archieve.showAllNotes())
     while (true) {
-        var noteID = scan.nextLine().toInt()
+        var noteID = archievePage.getMenuCorrectInput()
         when (noteID) {
             0 -> break
             else -> {
@@ -97,13 +84,12 @@ fun selectNote(archieve: Archieve) {
 
 fun createNote(archieve: Archieve){
     println("Создаем заметку")
-    val scan = Scanner(System.`in`)
 
     println("Введите название заметки:")
-    val noteName = scan.nextLine().toString()
+    val noteName = archievePage.getStringCorrectInput()
 
     println("Введите описание заметки:")
-    val noteDescription = scan.nextLine().toString()
+    val noteDescription = archievePage.getStringCorrectInput()
 
     archieve.addNote(Note(noteName, noteDescription))
     println("Заметка добавлена")
--- src/main/kotlin/PaigeLogic.kt
+++ src/main/kotlin/PaigeLogic.kt
@@ -1,36 +1,57 @@
 import Data.Archieve
+import java.util.Scanner
 
-class PaigeLogic{
+class PaigeLogic {
+    private val scan: Scanner = Scanner(System.`in`)
+
     var archieves = HashMap<Int, Archieve>()
 
-    fun isInputCorrect(value: String) : Boolean {
-        try {
-            value.toInt()
-            return true
-        } catch (e: NumberFormatException){
-            return false
+    fun getMenuCorrectInput(): Int {
+        var isInputNumber: Boolean
+
+        while (true) {
+            val value = scan.nextLine()
+            isInputNumber = try {
+                value.toInt()
+                true
+            } catch (e: NumberFormatException) {
+                false
+            }
+
+            if (!isInputNumber) {
+                println("Необходимо ввести число")
+                continue
+            }
+            return value.toInt()
         }
     }
 
-    fun createNewElement(){
-
+    fun getStringCorrectInput() : String{
+        while (true){
+            val value = scan.nextLine()
+            if (value.isBlank()){
+                println("Поле не может быть пустым, повторите ввод:")
+                continue
+            }
+            return value
+        }
     }
 
-    fun addArchieve(note: Archieve){
-        archieves[archieves.size+1] = note
+    fun addArchieve(note: Archieve) {
+        archieves[archieves.size + 1] = note
         println("Архив добавлен")
     }
 
-    fun showAllArchieves() : String? {
-        if(archieves.size > 0) {
-            for (archieve in archieves){
+    fun showAllArchieves(): String? {
+        if (archieves.size > 0) {
+            for (archieve in archieves) {
                 return archieve.key.toString() + ". " + archieve.value.name
             }
         }
         return null
     }
 
-    fun chooseArchieve(archieveID: Int) : Archieve? {
+    fun chooseArchieve(archieveID: Int): Archieve? {
         return archieves[archieveID]
     }
 }
